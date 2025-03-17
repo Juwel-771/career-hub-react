9ba8e4f3dbc6937eadf896cc2356bc603e9c290f
Juwel-771 <juwelah99@gmail.com>
2025-03-18 01:42:45 +0600

    data loaded by useState

--- src/components/FeaturedJob/FeaturedJob.jsx
+++ src/components/FeaturedJob/FeaturedJob.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Job from '../Job/Job';
 
 const FeaturedJob = () => {
+
+    const [jobs, setJobs] = useState(['']);
+
+    useEffect(()=>{
+        fetch('jobs.json')
+            .then(res => res.json())
+            .then(data => setJobs(data))
+    },[]);
+
     return (
         <div>
-            <h2 className='text-5xl text-center'>Featured Jobs</h2>
-            <p className='text-center'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            <div>
+                <h2 className='text-5xl text-center'>Featured Jobs {jobs.length} </h2>
+                <p className='text-center'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+            <div>
+                {
+                    jobs.map(job => <Job key={job.id} job = {job}></Job>)
+                }
+            </div>
         </div>
     );
 };
