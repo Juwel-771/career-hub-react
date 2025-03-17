5568175949a5b4fa4ca7141fb0ce972b2ffb39a6
Juwel-771 <juwelah99@gmail.com>
2025-03-18 02:46:57 +0600

    onsite and full time btn

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { IoLocationOutline } from "react-icons/io5";
+import { AiOutlineDollar } from "react-icons/ai";
+
 
 const Job = ({ job }) => {
   const { id, logo, job_title, company_name, remote_or_onsite, location, job_type, salary, job_description } = job;
@@ -13,8 +16,8 @@ const Job = ({ job }) => {
         <button className="px-5 py-2 font-extrabold border rounded border-[#7E90EF] mr-4 text-[#7E90EF]"> {job_type} </button>
     </div>
     <div className="flex text-gray-400">
-        <p> Location: {location} </p>
-        <p> Salary: {salary} </p>
+        <p className="flex items-center my-4"> <span className="mr-2"><IoLocationOutline/></span> Location: {location} </p>
+        <p className="flex items-center"> <span className="mr-2"> <AiOutlineDollar/> </span> Salary: {salary} </p>
     </div>
     <div className="card-actions">
       <button className="btn btn-primary">View Details</button>
